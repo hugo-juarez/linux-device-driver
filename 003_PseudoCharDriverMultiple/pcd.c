969b5351efcb54646f9eadfc55dd66d25100b7fc
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-07-15 13:07:28 -0500

    chore: Initialize multiple device pcd driver

---
+++ 003_PseudoCharDriverMultiple/pcd.c
@@ -0,0 +1,251 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+
+#undef pr_fmt
+#define pr_fmt(fmt)             "%s :" fmt, __func__
+
+#define NO_OF_DEVICES           4
+
+#define MEM_SIZE_MAX_PCDEV1     1024
+#define MEM_SIZE_MAX_PCDEV2     512
+#define MEM_SIZE_MAX_PCDEV3     1024
+#define MEM_SIZE_MAX_PCDEV4     512
+
+/* pseudo device's memory */
+char device_buffer_pcdev1[MEM_SIZE_MAX_PCDEV1];
+char device_buffer_pcdev2[MEM_SIZE_MAX_PCDEV2];
+char device_buffer_pcdev3[MEM_SIZE_MAX_PCDEV3];
+char device_buffer_pcdev4[MEM_SIZE_MAX_PCDEV4];
+
+/* Device private data structure */
+struct pcdev_private_data
+{
+    char * buffer;
+    unsigned size;
+    const char *serial_number;
+    int perm;
+    struct cdev cdev;
+};
+
+/* Driver private data structure */
+struct pcdrv_private_data
+{
+    int total_devices;
+    struct pcdev_private_data pcdev_data[NO_OF_DEVICES];
+};
+
+loff_t pcd_lseek (struct file *filp, loff_t offset, int whence);
+ssize_t pcd_read (struct file *filp, char __user *buff, size_t count, loff_t *f_pos);
+ssize_t pcd_write (struct file *filp, const char __user *buff, size_t count, loff_t *f_pos);
+int pcd_open (struct inode *inode, struct file *filp);
+int pcd_release (struct inode *inode, struct file *filp);
+
+dev_t device_number;
+
+struct cdev pcd_cdev;
+
+struct file_operations pcd_fops = {
+    .open = pcd_open,
+    .write = pcd_write,
+    .read = pcd_read,
+    .llseek = pcd_lseek,
+    .release = pcd_release,
+    .owner = THIS_MODULE
+};
+
+struct class *class_pcd;
+struct device *device_pcd;
+
+static int __init pcd_driver_init(void)
+{
+//     int ret;
+
+//     /* Dynamically allocate a device number */
+//     ret = alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+
+//     if(ret < 0)
+//     {
+//         pr_err("Alloc chrdev failed\n");
+//         goto out;
+//     }
+
+//     pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
+
+//     /* Initialize cdev with fops */
+//     cdev_init(&pcd_cdev, &pcd_fops);
+
+//     /* Register a device (cdev structure) with VFS */
+//     pcd_cdev.owner = THIS_MODULE;
+//     ret = cdev_add(&pcd_cdev, device_number, 1);
+
+//     if(ret < 0)
+//     {
+//         pr_err("Cdev add failed\n");
+//         goto unreg_chrdev;
+//     }
+
+//     /* Create device class under /sys/class/ */
+//     class_pcd = class_create("pcd_class");
+
+//     if(IS_ERR(class_pcd))
+//     {
+//         pr_err("Class creation failed\n");
+//         ret = PTR_ERR(class_pcd);
+//         goto cdev_del;
+//     }
+
+//     /* Populate the sysfs with device information */
+//     device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
+
+//     if(IS_ERR(device_pcd))
+//     {
+//         pr_err("Device creation failed\n");
+//         ret = PTR_ERR(device_pcd);
+//         goto class_del;
+//     }
+
+//     pr_info("Module init was successful \n");
+
+//     return 0;
+
+// class_del:
+//     class_destroy(class_pcd);
+
+// cdev_del:
+//     cdev_del(&pcd_cdev);
+
+// unreg_chrdev:
+//     unregister_chrdev_region(device_number, 1);
+
+// out:
+//     pr_err("Module insertion failed\n");
+//     return ret;
+    return 0;
+}
+
+static void __exit pcd_driver_cleanup(void)
+{
+    device_destroy(class_pcd, device_number);
+    class_destroy(class_pcd);
+    cdev_del(&pcd_cdev);
+    unregister_chrdev_region(device_number, 1);
+    pr_info("Module unloaded\n");
+}
+
+loff_t pcd_lseek (struct file *filp, loff_t offset, int whence)
+{
+    // loff_t temp;
+
+    // pr_info("lseek requested\n");
+    // pr_info("Current file position = %lld\n", filp->f_pos);
+
+    // switch(whence) 
+    // {
+    //     case SEEK_SET:
+    //         if( offset > DEV_MEM_SIZE || offset < 0 ) 
+    //         {
+    //             return -EINVAL;
+    //         }
+    //         filp->f_pos = offset;
+    //         break;
+    //     case SEEK_CUR:
+    //         temp = filp->f_pos + offset;
+    //         if( temp > DEV_MEM_SIZE || temp < 0 )
+    //         {
+    //             return -EINVAL;
+    //         }
+    //         filp->f_pos = temp;
+    //         break;
+    //     case SEEK_END:
+    //         temp = DEV_MEM_SIZE + offset;
+    //         if( temp > DEV_MEM_SIZE || temp < 0 )
+    //         {
+    //             return -EINVAL;
+    //         }
+    //         filp->f_pos = temp;
+    //         break;
+    //     default:
+    //         return -EINVAL;
+    // }
+
+    // pr_info("Updated file position = %lld\n", filp->f_pos);
+    // return filp->f_pos;
+    return 0;
+}
+ssize_t pcd_read (struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
+{
+    // pr_info("read requested for %zu bytes\n", count);
+    // pr_info("Current file position = %lld\n", *f_pos);
+
+    // /* Adjust the count */
+    // if( (*f_pos + count) > DEV_MEM_SIZE )
+    // {
+    //     count = DEV_MEM_SIZE - *f_pos;
+    // }
+
+    // /* Copy to user */
+    // if(copy_to_user(buff, &device_buffer[*f_pos], count))
+    // {
+    //     return -EFAULT;
+    // }
+
+    // /* Upadte current file position */
+    // *f_pos += count;
+
+    // pr_info("Number of bytes successfully read = %zu\n", count);
+    // pr_info("Updated file position = %lld\n", *f_pos);
+
+    // return count;
+    return 0;
+}
+ssize_t pcd_write (struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
+{
+    // pr_info("write requested for %zu bytes\n", count);
+    // pr_info("Current file position = %lld\n", *f_pos);
+
+    // /* Adjust the count */
+    // if( (*f_pos + count) > DEV_MEM_SIZE )
+    // {
+    //     count = DEV_MEM_SIZE - *f_pos;
+    // }
+
+    // if(!count) 
+    // {
+    //     return -ENOMEM;
+    // }
+
+    // /* Copy from user */
+    // if(copy_from_user(&device_buffer[*f_pos], buff, count))
+    // {
+    //     return -EFAULT;
+    // }
+
+    // /* Upadte current file position */
+    // *f_pos += count;
+
+    // pr_info("Number of bytes successfully written = %zu\n", count);
+    // pr_info("Updated file position = %lld\n", *f_pos);
+
+    // return count;
+    return 0;
+}
+int pcd_open (struct inode *inode, struct file *filp)
+{
+    pr_info("open was successful\n");
+    return 0;
+}
+int pcd_release (struct inode *inode, struct file *filp)
+{
+    pr_info("relea was successful\n");
+    return 0;
+}
+
+module_init(pcd_driver_init);
+module_exit(pcd_driver_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Hugo J");
+MODULE_DESCRIPTION("A pseudo character driver that handles n devices.");
+MODULE_INFO(board, "Beaglebone black REV A5");
